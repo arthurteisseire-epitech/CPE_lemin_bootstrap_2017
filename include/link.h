775fb5a827befd64c8819ce6c1f942702c506142
arthur <arthur.teisseire@epitech.eu>
2018-03-12 14:32:53 +0100

    lk_list done

---
+++ include/link.h
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2017
+** File Name : lk_list.h
+** File description:
+** by Arthur Teisseire
+*/
+
+#ifndef LK_LIST_H
+#define LK_LIST_H
+
+typedef struct link {
+	int data;
+	struct link *next;
+} link_t;
+
+#endif
--- src/main.c
+++ src/main.c
@@ -5,9 +5,57 @@
 ** by Arthur Teisseire
 */
 
+#include <stdlib.h>
+#include <stdio.h>
+#include "link.h"
 #include "my.h"
+
+link_t *create_link(int data)
+{
+	link_t *link = malloc(sizeof(link_t));
+
+	link->data = data;
+	return (link);
+}
+
+void print_link(link_t *link)
+{
+	printf("%d\n", link->data);
+}
+
+void connect_links(link_t *link1, link_t *link2)
+{
+	link1->next = link2;
+}
+
+void print_data_connected_links(link_t *link)
+{
+	while (link != NULL) {
+		print_link(link);
+		link = link->next;
+	}
+}
+
+void free_links(link_t *link)
+{
+	link_t *tmp;
+
+	while (link != NULL) {
+		tmp = link;
+		link = link->next;
+		free(tmp);
+	}
+}
 
 int main(void)
 {
+	link_t *link1 = NULL;
+	link_t *link2 = create_link(2);
+	link_t *link3 = create_link(3);
 
+	connect_links(link3, link2);
+	connect_links(link2, link1);
+	print_data_connected_links(link3);
+	free_links(link3);
+	return (0);
 }
